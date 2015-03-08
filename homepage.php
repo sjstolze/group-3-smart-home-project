9a9f57c178f3916abfc3e489494e6d37096e188f
slmadey <slmadey@email.uark.edu>
2015-03-08 11:52:48 -0500

    Adding homepage.php
    Okay, so I'm gonna make this a pull request so that we can work on it. I think this is how it works. Let me know if I need to do something other than a pull request.

---
+++ homepage.php
@@ -0,0 +1,49 @@
+<html>
+<?php
+	echo 'Username: ' . htmlspecialchars($_POST["username"]) . "<br>";
+	echo 'Email: ' . htmlspecialchars($_POST["email"]);
+	echo "<br>";
+	echo "<br>";
+?>
+<script>
+function runClient(){
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.onreadystatechange = function() {
+		if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+			var output = JSON.parse(xmlhttp.responseText);
+			document.getElementById("foo").innerHTML = output;
+		}
+	}
+	xmlhttp.open("POST", "runClient.php", true);
+	xmlhttp.send();
+};
+</script>
+
+<style>
+	div.red {border-style: solid; border-color: red; border-width: 10px;}
+	div.blue {border-style: groove; border-color: blue; border-width: 5px;}
+	div.indent {text-indent: 50px;}
+	ol.a {list-style-type: circle;}
+	ol.b {list-style-type: square;}
+</style>
+<body>
+	<div class="red">
+		DEVICE MANAGER
+		<div class = "blue">
+		Devices
+			<ol class = "a">
+				<li>Device 1</li>
+					<ol class = "b">
+						<li>Function 1</li>
+						<li>Function 2</li>
+						<li>Function 3</li>
+					</ol>
+				<li>Device 2</li>
+				<li>Device 3</li>
+			</ol>
+		<button onclick = "runClient();">Find new devices...</button>
+		<div id = "foo"><div>
+		</div>
+	</div>
+</body>
+</html>
